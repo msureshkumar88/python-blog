24ebb1b1bc0f49da0525a421b41a6ae8098d95c3
Suresh Kumar <suresh12_dmm@yahoo.com>
2022-09-08 16:28:20 +0100

    post list page add link to single post page

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_details, name="posts-details-page")
+    path("posts/<slug:slug>", views.post_details, name="post-details-page")
 ]
