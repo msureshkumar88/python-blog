ba8539b009a8c0d68ed582d7c397e77c55178a61
Suresh <suresh12_dmm@yahoo.com>
2022-09-08 10:38:44 +0100

    create url and view place holders for index, posts and posts details page

---
+++ my_site/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="home-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_details, name="posts-details-page")
+]
