268462e2660d3a606639748e00daed4bf788b151
Suresh Kumar <suresh12_dmm@yahoo.com>
2022-09-09 10:40:47 +0100

    create sample blog post details page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -11,5 +11,7 @@ def posts(request):
     return render(request, "blog/posts.html")
 
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    return render(request, "blog/post-details.html")
+
+# 5.10
