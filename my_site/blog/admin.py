a39e6c107ae4197952d2a5b35bde22ad19938414
Suresh Kumar <suresh12_dmm@yahoo.com>
2022-09-09 16:00:22 +0100

    reformat cod

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post, Author, Tag
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_filter = ("author","tags", "date")
+    list_filter = ("author", "tags", "date")
     list_display = ("title", "date", "author")
     prepopulated_fields = {"slug": ("title",)}
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
