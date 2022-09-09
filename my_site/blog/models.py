61c189de6186d152594342b8cf3e9457cba66263
Suresh Kumar <suresh12_dmm@yahoo.com>
2022-09-09 15:03:12 +0100

    Tag model add name

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -8,6 +8,10 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self) :
+        return self.caption
+
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
