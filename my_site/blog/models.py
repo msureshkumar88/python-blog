38475f50258b04aebc3e57276e1d47e9652d3c6e
Suresh Kumar <suresh12_dmm@yahoo.com>
2022-09-09 13:01:05 +0100

    add model class name in author

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -12,6 +12,12 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -19,7 +25,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
     
 
