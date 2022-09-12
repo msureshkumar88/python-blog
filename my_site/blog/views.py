fd70b5a46959485bfe7b803a91d9d56368a0a433
Suresh <suresh12_dmm@yahoo.com>
2022-09-12 15:18:16 +0100

    implement comment save page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView
+from .forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -22,11 +26,29 @@ class PostsView(ListView):
     context_object_name = "posts"
 
 
-class PostDetailsView(DetailView):
-    template_name = "blog/post-details.html"
-    model = Post
+class PostDetailsView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-details.html", context)
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["tags"] = self.object.tags.all()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-details-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-details.html", context)
