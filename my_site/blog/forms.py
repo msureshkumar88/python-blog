0e565b780a2eb0db2e99c9e0087ffc0ca45d1fb3
Suresh <suresh12_dmm@yahoo.com>
2022-09-28 16:10:57 +0100

    add class to commet form in form class

--- my_site/blog/forms.py
+++ my_site/blog/forms.py
@@ -12,3 +12,10 @@ class CommentForm(forms.ModelForm):
             "user_email": "Your Email",
             "text": "Your Comment"
         }
+        widgets = {
+            'user_name': forms.TextInput(attrs={'class': "form-control"}),
+            'user_email': forms.TextInput(attrs={'class': "form-control"}),
+            'text': forms.Textarea(attrs={'class': "form-control", "col":5, "row":5}),
+        }
+         
+                
