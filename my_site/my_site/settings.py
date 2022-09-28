22c8471619ca12b70249dbc47c60c580141b75b3
Suresh <suresh12_dmm@yahoo.com>
2022-09-28 16:14:00 +0100

    increase session timeout

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/files/"
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-SESSION_COOKIE_AGE = 120
+SESSION_COOKIE_AGE = 12000
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
